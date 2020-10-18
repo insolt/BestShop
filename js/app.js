1179e67f2e6b2412a92848c03ed409e02b93f4af
insolt <mike_born@hotmail.com>
2020-10-18 12:33:16 +0100

    Updated scss files

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const calcForm = document.forms[0].elements;
+const selectDropdown = document.querySelector('.select__dropdown');
+const summaryTotal = document.querySelector('.summary__total');
+const listItem = document.querySelector('.list__item');
+
+// selectDropdown.classList.add('open');
+summaryTotal.classList.add('open');
+listItem.classList.add('open');
+
+
+console.log(calcForm);
+
+console.log(calcForm.products);
+console.log(calcForm.orders);
+console.log(calcForm.accounting);
+console.log(calcForm.terminal);
